d9a8373bc22b9ab56d0e2e6a6294c20654c900e7
DairanDoo <your_email@example.com>
2025-03-28 21:01:54 -0400

    Login funcional implementado y navbar.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
 import { MatButtonModule } from '@angular/material/button'
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [MatButtonModule, RouterOutlet],
+  imports: [MatButtonModule, RouterOutlet, MatToolbarModule, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core'
+import { MatToolbar } from '@angular/material/toolbar'
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [MatToolbar, MatButtonModule, RouterLink],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css',
+  styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+}
